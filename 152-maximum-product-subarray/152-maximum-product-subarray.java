215ecb0d57995feef121eb4ad5f36374cf9004c8
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2022-05-02 22:03:10 +0530

    Time: 2 ms (71.11%), Space: 45.2 MB (28.93%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+ public int maxProduct(int[] nums) {
+        int max=nums[0], min=nums[0],prod=max;
+        for(int i=1;i<nums.length;i++){
+            
+            // If it is a negative number
+            if(nums[i]<0){
+                //swap max and min
+                int temp=max;
+                max=min;
+                min=temp;
+            }
+            max=Math.max(nums[i],nums[i]*max);
+            min=Math.min(nums[i],nums[i]*min);
+            if(prod<max) prod=max;
+        }
+        return prod;
+    }
+}
