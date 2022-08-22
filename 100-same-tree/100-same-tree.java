7ca4cd042ebe44296a94841a145425f00ba18205
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2022-08-22 23:44:06 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (61.13%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,22 +15,21 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
-        //base case 1
         if(p == null && q == null) {
             return true;
-        } 
-        // base case 2
+        }
+        
         else if(p == null || q == null) {
             return false;
         }
-        // base case 3
+        
         else if(p.val != q.val) {
             return false;
-        }
-        // recursive call
-        else {
+        } 
+        
+        else  {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
     }
+    
 }
